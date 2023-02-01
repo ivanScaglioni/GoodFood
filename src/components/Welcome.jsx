803ec983b03874ae7b140871342316137acbef33
ivanScaglioni <ivanscargentino@gmail.com>
2023-02-01 01:42:13 -0300

    Beta 1.4

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,9 +3,9 @@ import qr from "../assets/qrcode.png"
 
 function Welcome() {
   return (
-    <div className="w-full z-20  gap-5 h-[100vh] text-lg flex flex-col flex-wrap items-center justify-center content-center  bg-fuchsia-900">
-        <div className="text-5xl title">Good Food</div>
-        <div className="w-[87vw] max-w-[400px] text-2xl text-center">
+    <div className="w-full z-20  gap-5 min-h-[100vh] text-lg flex flex-col flex-wrap items-center justify-center content-center  bg-fuchsia-900">
+        <div className="text-5xl p-6 title title-welcome">Good Food</div>
+        <div className="w-[87vw] max-w-[400px] text-xl text-center">
             
             <p>
             Menu prototype for a restaurant designed for mobile and the user experience for more information visit their 
@@ -17,7 +17,8 @@ function Welcome() {
           
 
         <img src={qr} alt="" />
-        <p className="text-center">
+
+        <p className="text-center text-sm">
           Idea and creation by  
           <a className="text-orange-400" href="https://github.com/ivanScaglioni"> IVAN SCAGLIONI</a>
           
